5bcbd49637ef5f7f0139056ff92d27ab1820ece3
tommyrharper <thomasrobertharper@gmail.com>
2024-07-01 16:30:21 +0100

    👷🏻 Add IPaymaster interface to MarginPaymaster

--- src/MarginPaymaster.sol
+++ src/MarginPaymaster.sol
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
+import {IPaymaster, PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/IPaymaster.sol";
+
 /// @title Kwenta Paymaster Contract
 /// @notice Responsible for paying tx gas fees using trader margin
 /// @author tommyrharper (zeroknowledge@gmail.com)
-contract MarginPaymaster {
+contract MarginPaymaster is IPaymaster {
     uint256 public number;
+
+    function validatePaymasterUserOp(
+        PackedUserOperation calldata,
+        bytes32,
+        uint256
+    ) external returns (bytes memory context, uint256 validationData) {}
+
+    function postOp(PostOpMode, bytes calldata, uint256, uint256) external {}
 
     function increment() public {
         number++;
