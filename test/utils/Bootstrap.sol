7caa45da144852677876636f34e456945197c747
tommyrharper <thomasrobertharper@gmail.com>
2024-07-02 14:37:49 +0100

    ✅ Add user op to bootstrap

--- test/utils/Bootstrap.sol
+++ test/utils/Bootstrap.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.25;
 
-import {EntryPoint} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
-import {LightAccountFactory} from "lib/light-account/src/LightAccountFactory.sol";
+import {EntryPoint, PackedUserOperation} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
+import {LightAccountFactory, LightAccount} from "lib/light-account/src/LightAccountFactory.sol";
+import {BaseLightAccount} from "lib/light-account/src/common/BaseLightAccount.sol";
 
 import {console2} from "lib/forge-std/src/console2.sol";
-import {
-    MarginPaymaster,
-    OptimismGoerliParameters,
-    OptimismParameters,
-    Setup
-} from "script/Deploy.s.sol";
+import {MarginPaymaster, OptimismGoerliParameters, OptimismParameters, Setup} from "script/Deploy.s.sol";
 import {Test} from "lib/forge-std/src/Test.sol";
 
 contract Bootstrap is Test {
@@ -22,23 +18,49 @@ contract Bootstrap is Test {
 
     function initializeLocal() internal {
         BootstrapLocal bootstrap = new BootstrapLocal();
-        (address marginPaymasterAddress) = bootstrap.init();
+        address marginPaymasterAddress = bootstrap.init();
 
         marginPaymaster = MarginPaymaster(marginPaymasterAddress);
         entryPoint = new EntryPoint();
-        lightAccountFactory = new LightAccountFactory(address(this), entryPoint);
+        lightAccountFactory = new LightAccountFactory(
+            address(this),
+            entryPoint
+        );
+
+        uint256 accountSalt = 1;
+        bytes memory initCode = abi.encodeWithSelector(
+            LightAccountFactory.createAccount.selector,
+            address(this),
+            accountSalt
+        );
+        address dest = address(this);
+        uint256 value;
+        bytes memory func = abi.encodeWithSelector(MarginPaymaster.increment.selector);
+        bytes memory callData = abi.encodeWithSelector(BaseLightAccount.execute.selector, dest, value, func);
+        bytes memory signature;
+        PackedUserOperation memory op = PackedUserOperation({
+            sender: address(this),
+            nonce: 1,
+            initCode: initCode,
+            callData: callData,
+            accountGasLimits: bytes32(uint256(1 ether)),
+            preVerificationGas: 1 ether,
+            gasFees: bytes32(uint256(10 gwei)),
+            paymasterAndData: abi.encode(address(marginPaymaster)),
+            signature: signature
+        });
     }
 
     function initializeOptimismGoerli() internal {
         BootstrapOptimismGoerli bootstrap = new BootstrapOptimismGoerli();
-        (address marginPaymasterAddress) = bootstrap.init();
+        address marginPaymasterAddress = bootstrap.init();
 
         marginPaymaster = MarginPaymaster(marginPaymasterAddress);
     }
 
     function initializeOptimism() internal {
         BootstrapOptimismGoerli bootstrap = new BootstrapOptimismGoerli();
-        (address marginPaymasterAddress) = bootstrap.init();
+        address marginPaymasterAddress = bootstrap.init();
 
         marginPaymaster = MarginPaymaster(marginPaymasterAddress);
     }
