af8f6b0be6c6db124702bdaf3412b8bc6da016da
tommyrharper <thomasrobertharper@gmail.com>
2024-07-08 22:04:38 +0100

    ✅ Test on base fork url

--- test/MarginPaymaster.t.sol
+++ test/MarginPaymaster.t.sol
@@ -4,10 +4,11 @@ pragma solidity 0.8.25;
 import {Bootstrap} from "test/utils/Bootstrap.sol";
 
 contract MarginPaymasterTest is Bootstrap {
+    uint256 constant BASE_BLOCK_NUMBER = 16841532;
     function setUp() public {
         /// @dev uncomment the following line to test in a forked environment
         /// at a specific block number
-        // vm.rollFork(NETWORK_BLOCK_NUMBER);
+        vm.rollFork(BASE_BLOCK_NUMBER);
 
         initializeLocal();
     }
