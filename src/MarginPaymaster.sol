14317502da53f221ed757b90c8f7bb8701c84d8e
tommyrharper <thomasrobertharper@gmail.com>
2024-07-08 21:43:10 +0100

    👷🏻 Return context from validatePaymasterUserOp

--- src/MarginPaymaster.sol
+++ src/MarginPaymaster.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.25;
 
 import {IPaymaster, UserOperation} from "lib/account-abstraction/contracts/interfaces/IPaymaster.sol";
+import {console} from "forge-std/console.sol";
 
 /// @title Kwenta Paymaster Contract
 /// @notice Responsible for paying tx gas fees using trader margin
@@ -13,9 +14,19 @@ contract MarginPaymaster is IPaymaster {
         UserOperation calldata,
         bytes32,
         uint256
-    ) external returns (bytes memory context, uint256 validationData) {}
+    ) external returns (bytes memory context, uint256 validationData) {
+        // todo:
+        // assert(msg.sender == entryPoint)
+        console.log("validatePaymasterUserOp");
+        // context = new bytes(0); // passed to the postOp method
+        context = "yo"; // passed to the postOp method
+        validationData = 0; // special value means no validation
+    }
 
-    function postOp(PostOpMode, bytes calldata, uint256) external {}
+    function postOp(PostOpMode, bytes calldata, uint256) external {
+        // assert(msg.sender == entryPoint)
+        console.log("postOp");
+    }
 
     function increment() public {
         number++;
