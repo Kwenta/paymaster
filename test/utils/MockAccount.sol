84dce559a686b81b68abbc94849d7d45fb00e53e
tommyrharper <thomasrobertharper@gmail.com>
2024-07-16 19:46:41 +0100

    👷🏻 Move IEngine into external interfaces

--- test/utils/MockAccount.sol
+++ test/utils/MockAccount.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Create2.sol";
 import {IERC721Receiver} from "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 import {IPerpsMarketProxy} from "src/interfaces/external/IPerpsMarketProxy.sol";
-import {IEngine} from "src/interfaces/IEngine.sol";
+import {IEngine} from "src/interfaces/external/IEngine.sol";
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {console} from "forge-std/console.sol";
 
